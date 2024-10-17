a29df8fba5b75f027b9a7b20ea596ffb578b5885
Chris <christian.jacome.mora@udla.edu.ec>
2024-10-17 09:53:46 -0500

    Update: Add new find queries

--- src/main/java/com/api/task_management/persistance/TaskRepository.java
+++ src/main/java/com/api/task_management/persistance/TaskRepository.java
@@ -4,6 +4,11 @@ import com.api.task_management.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByStatus(String status);
+    List<Task> findByStartDateBetween(LocalDate startDate, LocalDate endDate);
 }
